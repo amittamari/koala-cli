34e494020ac150171afd5693bcc77933feb97ebf
Amit Tamari <15573913+amittamari@users.noreply.github.com>
2023-12-01 18:35:27 +0200

    init data_dir

--- koala.py
+++ koala.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-import typer
+from os import mkdir
 
 import install
-import update
 import lockfile
+import update
 import health
+from utils import data_dir
+
+import typer
 
 app = typer.Typer(name='koala-cli', no_args_is_help=True, help='CLI tool to manage KoalaVim')
 app.add_typer(install.app, name='install')
@@ -13,5 +16,12 @@ app.add_typer(update.app, name='update')
 app.add_typer(lockfile.app, name='lockfile')
 app.add_typer(health.app, name='health')
 
+def init():
+    try:
+        mkdir(data_dir())
+    except FileExistsError:
+        pass
+
 if __name__ == '__main__':
+    init()
     app()
